11a2253a1b3909e669919d0cfa290dbe68eb3394
이상진 <sangzin.lee@nhn.com>
2019-10-11 18:44:31 +0900

    Set scss (mixin, reset, common, order)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// import "../scss/styles.scss";
+import "../scss/styles.scss";
 
 // const testFunc = async () => await console.log("webpack bunling test");
 
--- webpack.config.js
+++ webpack.config.js
@@ -44,9 +44,7 @@ const config = {
     path: OUTPUT_DIR,
     filename: "[name].js"
   },
-  plugins: [
-    new ExtractCSS('styles.css')
-  ]
+  plugins: [new ExtractCSS('styles.css')]
 }
 
 module.exports = config;
